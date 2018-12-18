e6f5289842375cfd54a58e470967bd4635056312
kotaro_k <kotaro.k13@gmail.com>
2018-12-19 03:59:37 +0900

    update server logic

--- app/server/index.js
+++ app/server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
+// const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
@@ -25,7 +25,7 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, () => console.log(`Server is running on port ${port}`))
+  app.listen(port)
   // app.listen(port, host)
   // consola.ready({
   //   message: `Server listening on http://${host}:${port}`,
